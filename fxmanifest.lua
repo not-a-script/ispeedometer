c1764a2493f874c1439cdc96e572d2f236118ccf
Space V <40030799+TheSpaceGamerV2@users.noreply.github.com>
2022-04-26 22:44:39 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 use_fxv2_oal 'yes'
 lua54 'yes'
 author 'Akkariin'
-description 'Edited by Sinyx for iDev'
+description 'Edited by Sinyx & Space V for iDev'
 support 'https://discord.gg/8ecXhFXqR4'
 version '1.1'
 
